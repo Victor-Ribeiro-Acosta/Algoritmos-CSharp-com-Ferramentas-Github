f2eca7f8d37e6de2657c499c970a5f63bfc645a4
Victor-Ribeiro-Acosta <143853364+Victor-Ribeiro-Acosta@users.noreply.github.com>
2025-02-25 00:21:37 +0000

    Enviando projeto de criação de algoritmos em c# com codespace e github copilot

---
+++ Primeiros_passos/Introducao/introducao.cs
@@ -0,0 +1,30 @@
+// Abaixo está a definição do namespace. Namespaces são usados para organizar o código.
+namespace IntroducaoCSharp
+{
+    // A classe Program contém o ponto de entrada do programa.
+    class Program
+    {
+        // O método Main é o ponto de entrada do programa.
+        // É o primeiro método que é executado quando o programa é iniciado.
+        static void Main()
+        {
+            // Este comando imprime "Olá, Mundo!" no console.
+            Console.WriteLine("Olá, Mundo!");
+
+            // Abaixo está um exemplo de declaração de variáveis.
+            int numero1 = 10; // Declara uma variável inteira chamada 'numero' e atribui o valor 10 a ela.
+            string texto = "Exemplo de texto"; // Declara uma variável string chamada 'texto' e atribui um valor a ela.
+            int numero2 = 20; // Declara uma variável inteira chamada 'numero2' e atribui o valor 20 a ela.
+
+
+            // Este comando imprime o valor das variáveis no console.
+            Console.WriteLine("Número: " + numero1);
+            Console.WriteLine("Texto: " + texto);
+            Console.WriteLine("Número 2: " + numero2);
+
+
+        }
+    }
+}
+
+  
---
+++ Primeiros_passos/OperacoesAritimeticas/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+using System;
+
+class OperacoesAritmeticas
+{
+    static void Main()
+    {
+        double num1 = 10;
+        double num2 = 5;
+
+        // Adição
+        double adicao = num1 + num2;
+        Console.WriteLine($"Adição: {num1} + {num2} = {adicao}");
+
+        // Subtração
+        double subtracao = num1 - num2;
+        Console.WriteLine($"Subtração: {num1} - {num2} = {subtracao}");
+
+        // Multiplicação
+        double multiplicacao = num1 * num2;
+        Console.WriteLine($"Multiplicação: {num1} * {num2} = {multiplicacao}");
+
+        // Divisão
+        double divisao = num1 / num2;
+        Console.WriteLine($"Divisão: {num1} / {num2} = {divisao}");
+
+        // Potenciação
+        double potencia = Math.Pow(num1, num2);
+        Console.WriteLine($"Potenciação: {num1} ^ {num2} = {potencia}");
+    }
+}
